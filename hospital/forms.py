6fda61ebd28eb3d27dcc6d8603dfc9fe251eae65
sjpander <sjpander@iitk.ac.in>
2022-03-20 01:06:50 +0530

    Some Changes

--- hospital/forms.py
+++ hospital/forms.py
@@ -38,9 +38,6 @@ class PatientUserForm(forms.ModelForm):
         'password': forms.PasswordInput()
         }
 class PatientForm(forms.ModelForm):
-    #this is the extrafield for linking patient and their assigend doctor
-    #this will show dropdown __str__ method doctor model is shown on html so override it
-    #to_field_name this will fetch corresponding value  user_id present in Doctor model and return it
     assignedDoctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Name and Department", to_field_name="user_id")
     class Meta:
         model=models.Patient
@@ -71,6 +68,4 @@ class ContactusForm(forms.Form):
 
 
 
-#Developed By : sumit kumar
-#facebook : fb.com/sumit.luv
-#Youtube :youtube.com/lazycoders
+
--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -94,6 +94,4 @@ urlpatterns +=[
 
 ]
 
-#Developed By : sumit kumar
-#facebook : fb.com/sumit.luv
-#Youtube :youtube.com/lazycoders
+
